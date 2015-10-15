e6626ab3bbf5b20823c34fc06929c7fb48926f7b
henkrs1 <eahenke@gmail.com>
2015-10-15 11:25:26 -0500

    added nav links that scroll to respective sections

--- js/portfolio.js
+++ js/portfolio.js
@@ -13,6 +13,33 @@ var ViewModel = function() {
 
 	self.projectList = ko.observableArray([]);
 
+	self.navLinks = ko.observableArray([
+		{
+			name: 'Projects',
+			section: 'projects'
+
+		},
+		{
+			name: 'About',
+			section: 'about'
+		},
+		{
+			name: 'Contact',
+			section: 'contact'
+		}
+	]);
+
+	//Scroll to a section when its nav link is clicked
+	self.scrollTo = function(element, event) {
+		event.preventDefault();
+		console.dir(element);
+		var destination = element.section;	
+		
+		$('.html, body').animate({
+			scrollTop: $('.' + destination).offset().top
+		}, 1000);
+	}
+
 	function init() {
 		var temp = [];
 		projects.forEach(function(project) {
