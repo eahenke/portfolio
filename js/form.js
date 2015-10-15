935745f4cc3a70f446f2d7aca2268b8cde326b1a
henkrs1 <eahenke@gmail.com>
2015-10-14 22:06:41 -0500

    added loading animation to form.  contact form done

--- js/form.js
+++ js/form.js
@@ -1,11 +1,14 @@
 (function() {
 	var form = $('#contact-form')
 	var formResult = form.find('.form-result');
+	var buttonText = 'Send';
 
 	form.submit(function(e) {
 		e.preventDefault();
 		formResult.removeClass('success error');
 		var data = form.serialize();
+
+		loading(true);
 
 		$.ajax({
 			type: 'POST',
@@ -13,6 +16,7 @@
 			data: data
 
 		}).done(function(response) {
+			loading(false);
 			formResult.removeClass('error');
 			formResult.addClass('success');
 			formResult.text(response);
@@ -21,6 +25,7 @@
 			form.find('input, textarea').val('');
 
 		}).fail(function(data) {
+			loading(false);
 			formResult.removeClass('success');
 			formResult.addClass('error');
 			if(data.responseText !== '') {
@@ -30,4 +35,14 @@
 			}
 		});
 	});
+
+	function loading(bool) {
+		if(bool) {
+			form.find('.loader').show();
+			form.find('button').text('');
+		} else {
+			form.find('.loader').hide();
+			form.find('button').text(buttonText);
+		}
+	}
 })();
