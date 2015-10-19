ce486208cc512cc53635fbea16d657c9be5e911c
henkrs1 <eahenke@gmail.com>
2015-10-19 13:27:09 -0500

    added links to projects section, altered project model to include appropriate icons

--- js/model/projects.js
+++ js/model/projects.js
@@ -4,7 +4,8 @@ var projects = [
 		"description": "I'm also an artist.  This is the online portfolio for my stencil and spraypaint artwork.",
 		"tools": ["HTML", "CSS", "PHP", "Javascript", "jQuery", "Responsive"],
 		"link": "http://www.henkestencils.com",
-		"github": "",
+		"codeLink": "",
+		"codeLinkIcon": "github",
 		"imagePath": "images/projectscreenshots/henkestencils.jpg"
 	},
 
@@ -13,7 +14,8 @@ var projects = [
 		"description": "An interactive map displaying restaurants and bars in Chicago's Hyde Park neighborhood, along with rating and pricing information.  Users may click an establishment in the sidebar to highlight its location on the map.  The map marker also displays an information window displaying a street address and website.",
 		"tools": ["HTML", "CSS", "Javascript", "KnockoutJS", "Google Maps API", "Responsive"],
 		"link": "projects/eatdrinkhydepark",
-		"github": "http://github.com/eahenke/eatdrinkhydepark",
+		"codeLink": "http://github.com/eahenke/eatdrinkhydepark",
+		"codeLinkIcon": "github",
 		"imagePath": "images/projectscreenshots/eatdrinkhydepark.jpg"
 	},
 
@@ -23,7 +25,8 @@ var projects = [
 		"description": "A web app that generates CSS for a custom grid based on user input.  Users may use range inputs to select the number of columns, the space between columns, the space between rows, and the column padding.  A PHP script generates CSS that the user may copy and paste it into their project.",
 		"tools": ["HTML", "CSS", "PHP", "Javascript", "jQuery", "Sass"],
 		"link": "projects/easygrid",
-		"github": "http://github.com/eahenke/easygrid",
+		"codeLink": "http://github.com/eahenke/easygrid",
+		"codeLinkIcon": "github",
 		"imagePath": "images/projectscreenshots/easygrid.jpg"
 	},
 
@@ -32,7 +35,8 @@ var projects = [
 		"description": "Play the classic boardgame against an AI opponent.  The AI will randomly guess until it hits a ship, and will then hunt down the rest of the ship before moving on.  A player may choose to have their ships placed for them randomly, or they may select which ships to use and place them themselves.",
 		"tools": ["HTML", "CSS", "Javascript", "jQuery", "Sass"],
 		"link": "projects/battleship",
-		"github": "http://github.com/eahenke/battleship",
+		"codeLink": "http://github.com/eahenke/battleship",
+		"codeLinkIcon": "github",
 		"imagePath": "images/projectscreenshots/battleship.jpg"
 	},
 
@@ -41,7 +45,8 @@ var projects = [
 		"description": "A tool to select random cards for use in the popular Dominion card game.  Various options allow a user to require certain types of cards in their randomly generated deck.",
 		"tools": ["HTML", "CSS", "Javascript", "jQuery", "Responsive"],
 		"link": "projects/dominion",
-		"github": "https://github.com/eahenke/dominion",
+		"codeLink": "https://github.com/eahenke/dominion",
+		"codeLinkIcon": "github",
 		"imagePath": "images/projectscreenshots/dominion.jpg"
 	},
 	
@@ -50,7 +55,8 @@ var projects = [
 		"description": "A sliding, controllable image carousel.  The slider begins automatically, but users may skip to the next slide, go to the previous slide, or skip to a specific slide.",
 		"tools": ["HTML", "CSS", "Javascript", "jQuery", "Sass"],
 		"link": "projects/slider",
-		"github": "http://github.com/eahenke/slider",
+		"codeLink": "http://github.com/eahenke/slider",
+		"codeLinkIcon": "github",
 		"imagePath": "images/projectscreenshots/slider.jpg"
 	},
 
@@ -59,7 +65,8 @@ var projects = [
 		"description": "A puzzle game: unjumble fifteen sliding tiles to win.  The user may also select from a variety of puzzle images, puzzle sizes, and difficulty levels.",
 		"tools": ["HTML", "CSS", "Javascript"],
 		"link": "projects/fifteen",
-		"github": "http://github.com/eahenke/fifteen",
+		"codeLink": "http://github.com/eahenke/fifteen",
+		"codeLinkIcon": "github",
 		"imagePath": "images/projectscreenshots/fifteen.jpg"
 	},
 ]
